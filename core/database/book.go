096c406e58e3e755c344127554bde6599f64198e
Mukul Mantosh <mukul.mantosh@jetbrains.com>
2024-03-04 15:25:27 +0530

    Models, GORM, Controllers

--- core/database/book.go
+++ core/database/book.go
@@ -6,6 +6,11 @@ import (
 )
 
 func (c Client) AddBook(ctx context.Context, book *models.Book) (*models.Book, error) {
+	var maxID int
+	if result := c.db.Model(&models.Book{}).Select("COALESCE(MAX(id), 0)").Scan(&maxID); result.Error != nil {
+		panic("failed to get max id")
+	}
+	book.ID = uint(maxID) + 1
 	result := c.db.WithContext(ctx).Create(&book)
 	if result.Error != nil {
 		return nil, result.Error
--- models/book.go
+++ models/book.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Book struct {
 	gorm.Model
-	Id              string `json:"Id" gorm:"primaryKey"`
+	Id              int64  `json:"ID" gorm:"primaryKey"`
 	Title           string `json:"title"`
 	ISBN            string `json:"isbn"`
 	PublicationDate string `json:"publication_date"`
