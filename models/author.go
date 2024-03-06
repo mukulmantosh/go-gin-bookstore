c42b4cdfd5b634b2baec0ae59a9dd33564377362
Mukul Mantosh <mukul.mantosh@jetbrains.com>
2024-03-06 20:40:13 +0530

    Add image field to author model.

--- models/author.go
+++ models/author.go
@@ -6,5 +6,6 @@ type Author struct {
 	gorm.Model
 	Id    string `json:"Id" gorm:"primaryKey"`
 	Name  string `json:"name"`
+	Image string `json:"image"`
 	Books []Book `gorm:"many2many:author_books;"`
 }
